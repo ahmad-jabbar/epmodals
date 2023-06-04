36b49c751fde4c1db0b856e6fd9f64b99dcf22da
Ahmad Jabbar <117609159+ahmad-jabbar@users.noreply.github.com>
2023-06-04 12:51:50 -0700

    Update experiment.js

--- stakes_qud/experiment.js
+++ stakes_qud/experiment.js
@@ -1,7 +1,7 @@
 const jsPsych = initJsPsych({
   on_finish: function(data){
     // jsPsych.data.displayData('csv');
-    proliferate.submit({"trials": data.values()});
+//     proliferate.submit({"trials": data.values()});
     window.location = "https://app.prolific.co/submissions/complete?cc=COH7SM62"
   }
 });
