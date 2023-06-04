3709ae8035ff93096d4afab20bc1b613a9ddf29d
Ahmad Jabbar <117609159+ahmad-jabbar@users.noreply.github.com>
2023-06-04 11:58:13 -0700

    Update experiment.js

--- stakes_qud/experiment.js
+++ stakes_qud/experiment.js
@@ -6,15 +6,15 @@ const jsPsych = initJsPsych({
        }
 });
 
-// var subject_id = jsPsych.data.getURLVariable('PROLIFIC_PID');
-// var study_id = jsPsych.data.getURLVariable('STUDY_ID');
-// var session_id = jsPsych.data.getURLVariable('SESSION_ID');
+var subject_id = jsPsych.data.getURLVariable('PROLIFIC_PID');
+var study_id = jsPsych.data.getURLVariable('STUDY_ID');
+var session_id = jsPsych.data.getURLVariable('SESSION_ID');
 
-// jsPsych.data.addProperties({
-//   subject_id: subject_id,
-//   study_id: study_id,
-//   session_id: session_id
-// });
+jsPsych.data.addProperties({
+  subject_id: subject_id,
+  study_id: study_id,
+  session_id: session_id
+});
 
 
 
